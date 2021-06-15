6a64ca23508b8ef391ff3c217f37b5a91b2b4f26
Geovany Lagos <geovanylagos95@gmail.com>
2021-06-15 17:40:15 -0600

    First commit

---
+++ areatriangulo.py
@@ -0,0 +1,5 @@
+print ("CALCULAR EL AREA DE UN TRIANGULO")
+base=input("Cual es la base: ")
+altura=input("Cual es la altura: ")
+area=int (base) * int (altura) / 2.0
+print ("el resultado es: " + str (area))
